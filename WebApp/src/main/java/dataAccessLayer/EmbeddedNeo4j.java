e0582622fa3987cc3d64892136ae5e6b9ec736c9
LeoConQui <con18797@uvg.edu.gt>
2023-05-22 19:47:08 -0600

    getBooksbyAuthor, insertPerson e insertBook

--- WebApp/src/main/java/dataAccessLayer/EmbeddedNeo4j.java
+++ WebApp/src/main/java/dataAccessLayer/EmbeddedNeo4j.java
@@ -144,6 +144,41 @@ public class EmbeddedNeo4j implements AutoCloseable{
         }
    }*/
 
+   /**
+    * getBooksbyAuthor es un metodo que obtiene los libros escritos por un escritor particular
+    * @param author es el autor del libro
+    * @return un Linked List de String con los libros escritos por el autor
+    */
+   public LinkedList<String> getBooksbyAuthor(String author)
+    {
+   	 try ( Session session = driver.session() )
+        {
+   		 
+   		 
+   		 LinkedList<String> books = session.readTransaction( new TransactionWork<LinkedList<String>>()
+            {
+                @Override
+                public LinkedList<String> execute( Transaction tx )
+                {
+                    //Result result = tx.run( "MATCH (tom:Person {name: \"" + actor + "\"})-[:ACTED_IN]->(actorMovies) RETURN actorMovies.title");
+                    Result result = tx.run( "MATCH(libro:Book{name: \"" + author+"\"}) RETURN libro.name");
+                    LinkedList<String> mybooks = new LinkedList<String>();
+                    List<Record> registros = result.list();
+                    for (int i = 0; i < registros.size(); i++) {
+                   	 //myactors.add(registros.get(i).toString());
+                   	 mybooks.add(registros.get(i).get("libro.name").asString());
+                    }
+                    
+                    return mybooks;
+                }
+            } );
+            
+            return books;
+        }
+   }
+
+
+
    /** getBooksbyLastRead es un metodo que recomiendo libros basado en la ultima lectura realizada
     * @param lastread es el nombre de la ultima lectura realizada por el usuario
     * @return un LinkedList con los nombres de las recomendaciones de libros
@@ -215,7 +250,7 @@ public class EmbeddedNeo4j implements AutoCloseable{
    }
 
      
-    public String insertMovie(String title, int releaseYear, String tagline) {
+    /*public String insertMovie(String title, int releaseYear, String tagline) {
     	try ( Session session = driver.session() )
         {
    		 
@@ -226,6 +261,69 @@ public class EmbeddedNeo4j implements AutoCloseable{
                 public String execute( Transaction tx )
                 {
                     tx.run( "CREATE (Test:Movie {title:'" + title + "', released:"+ releaseYear +", tagline:'"+ tagline +"'})");
+                    
+                    return "OK";
+                }
+            }
+   		 
+   		 );
+            
+            return result;
+        } catch (Exception e) {
+        	return e.getMessage();
+        }
+    }*/
+
+    /**
+     * insertBook es el metodo que agrega un libro como nodo a la base de datos
+     * @param author es el nombre del autor del lubro
+     * @param name es el nombre del libro
+     * @param editorial es el nombre de la editorial del libro
+     * @param genre es el genero del libro
+     * @return un String que indica que se realizo exitosamente la adicion
+     */
+    public String insertBook(String author, String name, String editorial, String genre) {
+    	try ( Session session = driver.session() )
+        {
+   		 
+   		 String result = session.writeTransaction( new TransactionWork<String>()
+   		 
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    //tx.run( "CREATE (Test:Movie {title:'" + title + "', released:"+ releaseYear +", tagline:'"+ tagline +"'})");
+                    tx.run( "CREATE (book:Book {author:'" + author + "', name:"+ name +", editorial:'"+ editorial +"', genre:" + genre +"})");
+                    
+                    return "OK";
+                }
+            }
+   		 
+   		 );
+            
+            return result;
+        } catch (Exception e) {
+        	return e.getMessage();
+        }
+    }
+
+    /**
+     * insertPerson es el metodo que ingresa una persona como nodo a la base de datos
+     * @param name es el nombre de la persona
+     * @return un string que indica que se realizo la insercion
+     */
+    public String insertPerson(String name) {
+    	try ( Session session = driver.session() )
+        {
+   		 
+   		 String result = session.writeTransaction( new TransactionWork<String>()
+   		 
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    //tx.run( "CREATE (Test:Movie {title:'" + title + "', released:"+ releaseYear +", tagline:'"+ tagline +"'})");
+                    tx.run( "CREATE (person:Person {name:'" + name + "'})");
                     
                     return "OK";
                 }
