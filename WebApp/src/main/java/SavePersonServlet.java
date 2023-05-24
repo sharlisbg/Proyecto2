6e16c6b264c81b909f8bb125186636e55debb26c
LeoConQui <con18797@uvg.edu.gt>
2023-05-23 20:48:44 -0600

    SavePerson

---
+++ WebApp/src/main/java/SavePersonServlet.java
@@ -0,0 +1,73 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import dataAccessLayer.EmbeddedNeo4j;
+
+/**
+ * Servlet implementation class SaveMovieServlet
+ */
+@WebServlet("/SavePersonServlet")
+public class SavePersonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SavePersonServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+	 	response.setContentType("application/json");
+	 	response.setCharacterEncoding("UTF-8");
+	 	JSONObject myResponse = new JSONObject();
+	 	
+	 	JSONArray insertionResult = new JSONArray();
+	 	
+	 	String personName = request.getParameter("name");
+	 	
+	 	// preguntar si se debe cambiar algo aqui
+	 	 try ( EmbeddedNeo4j neo4jDriver = new EmbeddedNeo4j( "bolt://44.213.125.209:7687", "neo4j", "maintenance-glossary-goal") )
+	        {
+			 	String myResultTx = neo4jDriver.insertPerson(personName);
+	        	
+			 	myResponse.put("resultado", myResultTx);
+	        } catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				myResponse.put("resultado", "Error: " + e.getMessage());
+			}
+	 	
+	 	
+	 	out.println(myResponse);
+	 	out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
