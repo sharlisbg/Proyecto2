eb57c862fd5e3830874bd6409d40fa5b42196c2b
sharlisbg <sharislizza15@gmail.com>
2023-05-23 21:30:45 -0600

    Revert "Cambios a java"
    This reverts commit f69497d1015b3c956c303273668a1f549b407f9d, reversing
    changes made to f0b3efa3db506b9505cda0f570655fc8daa24d61.

--- WebApp/src/main/java/SavePersonServlet.java
+++ WebApp/src/main/java/SavePersonServlet.java
@@ -1,8 +1,4 @@
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b1a8bb6c571c50757617d745ea84b95fb596b647
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.LinkedList;
@@ -19,11 +15,7 @@ import org.json.simple.JSONObject;
 import dataAccessLayer.EmbeddedNeo4j;
 
 /**
-<<<<<<< HEAD
  * Servlet implementation class SavePersonServlet
-=======
- * Servlet implementation class SaveMovieServlet
->>>>>>> b1a8bb6c571c50757617d745ea84b95fb596b647
  */
 @WebServlet("/SavePersonServlet")
 public class SavePersonServlet extends HttpServlet {
@@ -49,20 +41,11 @@ public class SavePersonServlet extends HttpServlet {
 	 	
 	 	JSONArray insertionResult = new JSONArray();
 	 	
-<<<<<<< HEAD
 	 	String name = request.getParameter("name");
 	 	
 	 	 try ( EmbeddedNeo4j neo4jDriver = new EmbeddedNeo4j( "bolt://44.215.127.186:7687", "neo4j", "elapse-career-realignments" ) )
 	        {
 			 	String myResultTx = neo4jDriver.insertPerson(name);
-=======
-	 	String personName = request.getParameter("name");
-	 	
-	 	// preguntar si se debe cambiar algo aqui
-	 	 try ( EmbeddedNeo4j neo4jDriver = new EmbeddedNeo4j( "bolt://44.213.125.209:7687", "neo4j", "maintenance-glossary-goal") )
-	        {
-			 	String myResultTx = neo4jDriver.insertPerson(personName);
->>>>>>> b1a8bb6c571c50757617d745ea84b95fb596b647
 	        	
 			 	myResponse.put("resultado", myResultTx);
 	        } catch (Exception e) {
@@ -84,9 +67,4 @@ public class SavePersonServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> b1a8bb6c571c50757617d745ea84b95fb596b647
